e2b3ea8a1c468fb1e963ff63062a799d6dd5fd9e
Jose David <lujan9495@gmail.com>
2024-04-05 11:58:10 -0500

    Heredar un type

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,10 +1,10 @@
 import { useState, useEffect, useMemo } from "react"
 import { db } from '../data/db'
-
+import type { CartItem } from "../types/types"
 
 export const useCart = () => {
   
-    const intialCart = () => {
+    const intialCart = () : CartItem[] => {
         const localStorageCart = localStorage.getItem('cart') 
         return localStorageCart ? JSON.parse(localStorageCart) : []
     }
